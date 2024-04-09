44d4aac67bf4a24ec4973704e666ca4eace7dbd4
zhuweilog <zhuweilong1127@qq.com>
2024-04-09 18:10:51 +0800

    框架构建项目提交

--- express框架内容/src/index.js
+++ express框架内容/src/index.js
@@ -9,7 +9,26 @@ app.get('/home', (req, res) => {
 
 // 首页路由
 app.get('/', (req, res) => {
+  // 设置响应头
+  res.set({
+    'Content-Type': 'text/plain;charset=uft-8'
+  })
   res.end('hello 我是首页')
+})
+
+// 首页路由
+app.get('/:id', (req, res) => {
+  console.log('获取的id',req.params.id)
+  // 设置响应头
+  res.set({
+    'Content-Type': 'text/plain;charset=uft-8'
+  })
+  res.end('hello 我是带id查询的接口')
+})
+
+// 创建一个home的post
+app.post('/home', (req, res) => {
+  res.end('这是一个post请求')
 })
 
 app.listen(3000, () => {
