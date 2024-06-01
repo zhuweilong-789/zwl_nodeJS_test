62d06d22c5ce7d8a6f58e2f296f2be2210ecf4ec
zhuweilong <zhuweilong1127@qq.com>
2024-06-01 14:59:11 +0800

    添加内容

--- express框架内容/src/01基础内容.js
+++ express框架内容/src/01基础内容.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   // 获取请求的参数, req下面有很多请求参数的属性
   console.log(req.path)
   console.log(req.query)
+  res.send('zhuweilong')
 })
 
 app.listen(3000, () => {
