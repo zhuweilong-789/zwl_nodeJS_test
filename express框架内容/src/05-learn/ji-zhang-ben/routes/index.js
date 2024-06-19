d0f8f86a81714613e45d211305a016fd664366a7
zhuweilog <zhuweilong1127@qq.com>
2024-06-19 17:58:35 +0800

    添加记账本接口

--- express框架内容/src/05-learn/ji-zhang-ben/routes/index.js
+++ express框架内容/src/05-learn/ji-zhang-ben/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 记账本列表
+router.get('/account', function(req, res, next) {
+  res.send('账本列表')
 });
+
+// 添加记录
+router.post('/account/create', function(req, res, next) {
+  res.send('添加列表')
+});
+
 
 module.exports = router;
